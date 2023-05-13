e1a3197703b3dbbddf84ff6fb0ffeb365d9b51c8
KietHo1 <kiethostartup1999@gmail.com>
2023-05-13 21:29:36 +0700

    Part 016 - Audio & Sound Effects

--- Assets/Scenes/Scripts/CoinCollect.cs
+++ Assets/Scenes/Scripts/CoinCollect.cs
@@ -6,7 +6,16 @@ public class CoinCollect : MonoBehaviour
 {
     private void OnTriggerEnter(Collider info)
     {
-        Destroy(gameObject);
+        AudioSource coinaudio = GetComponent<AudioSource>();
+        coinaudio.Play();
         CoinSystem.coinscollect += 1;
+        transform.position = new Vector3(0, -1000, 0);
+        StartCoroutine(CarryOn());//Run another command below
+    }
+
+    IEnumerator CarryOn()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(gameObject);
     }
 }
